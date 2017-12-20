aac515adb0e3c080a6fa698c32de09cab06f3066
ivanwarke <iveon.cc@gmail.com>
2017-12-20 11:21:32 +0800

    package

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const sourcePath = path.resolve(__dirname, 'src');
+const buildPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: path.resolve(sourcePath, 'index.js'),
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        },
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.coffee'],
+  },
+};
