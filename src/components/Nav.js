e521a1950c7901dedc6d70cbea46ab18474160a6
ivanwarke <iveon.cc@gmail.com>
2017-12-23 12:32:35 +0800

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,11 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import CategoryIcon from 'material-ui/svg-icons/content/content-copy';
+import FoodIcon from 'material-ui/svg-icons/maps/local-pizza';
+import OrdersIcon from 'material-ui/svg-icons/editor/insert-drive-file';
+import CouponIcon from 'material-ui/svg-icons/action/receipt';
+import UserIcon from 'material-ui/svg-icons/action/supervisor-account';
 
 class Nav extends React.Component {
   constructor(props) {
@@ -17,11 +22,11 @@ class Nav extends React.Component {
       <div>
         <Paper style={styles.paper}>
           <Menu menuItemStyle={styles.menu}>
-            <MenuItem primaryText="菜单" />
-            <MenuItem primaryText="分类" />
-            <MenuItem primaryText="订单" />
-            <MenuItem primaryText="优惠券" />
-            <MenuItem primaryText="用户" />
+            <MenuItem primaryText="菜单" rightIcon={<FoodIcon />} />
+            <MenuItem primaryText="分类" rightIcon={<CategoryIcon />} />
+            <MenuItem primaryText="订单" rightIcon={<OrdersIcon />} />
+            <MenuItem primaryText="优惠券" rightIcon={<CouponIcon />} />
+            <MenuItem primaryText="用户" rightIcon={<UserIcon />} />
           </Menu>
         </Paper>
       </div>
