75c0d44e77a5d965bf2562cde8b9241a8a85cc5c
ivanwarke <iveon.cc@gmail.com>
2018-03-07 19:01:17 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,27 +2,27 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Routes from './routes';
-import allReducers from './reducers';
+// import allReducers from './reducers';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './components/styles/index.less';
 
 injectTapEventPlugin();
-const store = createStore(allReducers);
+// const store = createStore(allReducers);
 
 ReactDOM.render(
-  <Provider store={store}>
+  // <Provider store={store}>
     <MuiThemeProvider>
       <BrowserRouter basename='/'>
         <Routes />
       </BrowserRouter>
-    </MuiThemeProvider>
-  </Provider>,
+    </MuiThemeProvider>,
+  // </Provider>,
   document.getElementById('root')
 );
 
